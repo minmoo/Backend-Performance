e989831e0d17e949f1df02754643f0fb96fce4b8
minmoo <kimminsu951@hanmail.net>
2021-04-14 06:25:29 +0000

    django

---
+++ pythonDjango/performance/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+
+    # DB조회
+
+    # HTTP 요청
+
+    # FILE I/O
+
+
+
+
+
+    return HttpResponse("Hello")
